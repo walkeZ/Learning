43b0ce0b78eb825768c0795d0015036b806f2331
walke <1126648815@qq.com>
2018-06-08 16:50:49 +0800

    添加了python基本输出输入练习文件

---
+++ Python/1806/BaseTest/test_time02.py
@@ -0,0 +1,72 @@
+﻿# _*_ coding=utf-8 _*_  	# 编码声明 
+# 参考： 
+#	python初步学习-import和datetime模块 ：https://www.cnblogs.com/pingqiang/p/7812137.html
+from datetime import datetime , timedelta
+
+now=datetime.now() 	# 获取当前的datetime ：
+print ("now= ",now)			# 2018-06-08 10:33:53.238822
+
+input()
+
+now+=timedelta(hours=5)
+print("now+=timedelta(hours=5): ",now)
+
+input()
+
+now+=timedelta(hours=-2)
+print("now+=timedelta(hours=-2): ",now)
+
+input()
+
+now-=timedelta(hours=1)
+print("now-=timedelta(hours=1): ",now)
+
+input()
+
+now-=timedelta(days=1)
+print("now+=timedelta(days=1): ",now)
+
+# 获取指定日期和时间 SyntaxError: invalid token
+#dt=datetime(2018,06,08,10,55)
+dt=datetime(2018,6,8,10,55)
+print(dt)
+
+print("\ndatetime转换为str",now.strftime('%a,%b,%Y-%m-%d %H:%I:%M'))
+print("\ndatetime转换为str",now.strftime('%a,%b,%Y年%m月%d %H:%I:%M'))	# 报错了
+# UnicodeEncodeError: 'locale' codec can't encode character '\u5e74' in position 8: Illegal byte sequence
+'''  duohang
+python中时间日期格式化符号：
+%y 两位数的年份表示（00-99）
+%Y 四位数的年份表示（000-9999）
+%m 月份（01-12）
+%d 月内中的一天（0-31）
+%H 24小时制小时数（0-23）
+%I 12小时制小时数（01-12）
+%M 分钟数（00=59）
+%S 秒（00-59）
+
+%a 本地简化星期名称
+%A 本地完整星期名称
+%b 本地简化的月份名称
+%B 本地完整的月份名称
+%c 本地相应的日期表示和时间表示
+%j 年内的一天（001-366）
+%p 本地A.M.或P.M.的等价符
+%U 一年中的星期数（00-53）星期天为星期的开始
+%w 星期（0-6），星期天为星期的开始
+%W 一年中的星期数（00-53）星期一为星期的开始
+%x 本地相应的日期表示
+%X 本地相应的时间表示
+%Z 当前时区的名称
+%% %号本身
+
+'''
+
+input()
+
+
+
+
+
+
+
---
+++ Python/1806/base_print_input.py
@@ -0,0 +1,52 @@
+﻿# _*_ coding:utf-8 _*_
+# 视频地址：https://v.qq.com/x/cover/9v3r1r0sdbyv41o/h01266d7rgp.html
+print(10) 						# 控制台输出10 			输出int数据
+print(10.11) 					# 输出 10.11  			float数据
+print('H') 						# 输出 H  				char(字符)数据
+print('Hello, World!,学习') 	# 输出 Hello, World! 	String 数据
+print("H")         				# 输出 Hello			char数据
+print("Hello, World!，学习")	# 输出 Hello, World!   	string数据
+print("换行: \n第二行")			# 输出 换行：(换行后第二行)
+print("隔一行",'111111')		# 输出 隔一行 111111	两个参数的print函数第二个参数可不写，有个空格隔开
+a=10							# "="(等号)：赋值语句,定义了一个变量a并给其赋值为10；此时a为int类型
+print('a=',a)						# 语义：输出(打印) 变量a
+b=12.34							# 定义了一个变量b并赋值为12.34；此时b为float类型
+print("b=",b)
+a='hello'						# 定义了一个遍a并赋值为'hello'；此时a为string类型，会冲掉原来的a=10 
+print('a=',a)
+print("\n\n")
+# 格式化输出 print(format(val,format_modifier)) ;
+# val(参数一) 一般为浮点(float[单精度],double[双精度])值,参数二：调节器(即为目标格式)
+# format_modifier：【m.nf】,m:占位，n:小数个数，f:flaot类型，当m>格式后的数据长度则右对齐，反之靠左，若不够往后补
+#当n<小数个数，四舍五入，n>则补0；没有f则默认返回double类型，格式化输出(带e)
+z=12.345678
+print(format(z,'6.2f'))			# 结果：“ 12.34”
+print(format(z,"6.2f"))			# 结果：“ 12.34”
+print(format(z,'6.2')) 			# 结果：“1.2e+01”
+print(format(z,'6.5f')) 		# 结果：“12.34568”
+print(format(z,'6.9f')) 		# 结果：“12.345678000”
+print(format(z,'6.0f')) 		# 结果：“    12”
+# %数据 具体输出规律同上
+y=0.123456
+print(format(y,'.2%'))			# “12.35%”
+print(format(y,'7.2%'))			# “ 12.35%”
+
+#输入input()，已enter结束输入
+d=input() 						# 返回默认为string类型，故无法直接执行d+=10
+print("input：d 为 ",d)
+print('type(d): ',type(d))					# 查看d的类型
+# d+=10		TypeError: must be str, not int		
+d=int(d)						# 强转  注意输入类型，
+#强转 输入“hello”报错：ValueError: invalid literal for int() with base 10: 'hello'
+d+=10
+print("input：d+=10 为 ",d)
+age=int(input("请输入XX(age)"))
+age-=1
+print('age= ',age,'\n','再换两行','111\n','222\n')  # print函数参数估计不限个数
+weight=float(input("请输入XX(weight[float])"))
+weight-=1
+print('weight= ',weight)
+input() #输入 可实现python代码暂停执行，按enter键继续
+
+
+
