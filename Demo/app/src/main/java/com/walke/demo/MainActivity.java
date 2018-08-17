fb7654176b5ed8e2735275f7d3e64f881cbc0308
walke <1126648815@qq.com>
2018-08-17 14:50:45 +0800

    对1808.txt做了记录添加，在Demo项目中做了一个小验证(View中的context是不是(能不能强转为)Activity)

--- Demo/app/src/main/java/com/walke/demo/MainActivity.java
+++ Demo/app/src/main/java/com/walke/demo/MainActivity.java
@@ -1,7 +1,9 @@
 package com.walke.demo;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.i("walke:   MainActivity", "   onCreate:-------> this；"+this);
+        Context context = findViewById(R.id.main_tv).getContext();
+        Log.i("walke:   MainActivity", "   onCreate:-------> context: "+context);
+
     }
 }
