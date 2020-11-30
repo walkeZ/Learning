e2858eeed5e4c6e9caa521454767c4fd20182233
walke <1126648815@qq.com>
2020-11-30 16:34:44 +0800

    main包主框架(navigator activity)的补充

--- KtPractice/app/src/main/java/com/walke/ktpractice/main/MainActivity.kt
+++ KtPractice/app/src/main/java/com/walke/ktpractice/main/MainActivity.kt
@@ -9,6 +9,9 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.walke.ktpractice.R
 
+/**
+ * main包中完全就是复制创建项目是选中Navigation Activity。。。
+ */
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,7 +24,11 @@ class MainActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(setOf(
             R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications))
+        // 报错了： Caused by: java.lang.NullPointerException: Attempt to invoke virtual method 'void androidx.appcompat.app.ActionBar.setTitle(java.lang.CharSequence)' on a null object
+        // 断点进源码发现：ActionBarOnDestinationChangedListener的setTitle方法中actionBar为null了
+        // 这个报错与主题有关。之前默认主题是：...NoActionBar.
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
     }
 }
