f1fb1d8445565d893036340626bbfe68def5f128
walke <1126648815@qq.com>
2024-11-30 15:03:08 +0800

    新建项目AGP8环境demo，主要是查看对比gradle文件结构语法差异

---
+++ AndroidProject/AGP8_Demo1/app/src/main/java/com/walker/agp8_demo1/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.walker.agp8_demo1
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.walker.agp8_demo1.ui.theme.AGP8_Demo1Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            AGP8_Demo1Theme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    AGP8_Demo1Theme {
+        Greeting("Android")
+    }
+}
